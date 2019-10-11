d290ef27ed1a0a6a96a0a791efee46ffd68af946
Gianguido Sorà <me@gsora.xyz>
2019-10-11 17:09:36 +0200

    make reverse work

--- garbage/reverse.go
+++ garbage/reverse.go
@@ -1,42 +1,44 @@
 package garbage
 
-// Get a string and reverse it
+import "unicode"
+
+// Reverse gets a string and reverse it
 func Reverse(text string) string {
-    characters := []rune(text)
-    charsLength := len(characters)
-    
-    // Single char cannot be reversed duh!
-    if charsLength <= 1 {
-        return text
-    }
+	characters := []rune(text)
+	charsLength := len(characters)
 
-    i, j := 0, 0
-    for i < charsLength && j < charsLength {
-        j = i + 1
-        for j < charsLength && isMark(characters[j]) {
-            j++
-        }
+	// Single char cannot be reversed duh!
+	if charsLength <= 1 {
+		return text
+	}
 
-        if isAMarker(characters[j-1]) {
-            // Reverse combined chars
-            reverse(characters[i:j], j-i)
-        } 
+	i, j := 0, 0
+	for i < charsLength && j < charsLength {
+		j = i + 1
+		for j < charsLength && isAMarker(characters[j]) {
+			j++
+		}
 
-        i = j
-    }
+		if isAMarker(characters[j-1]) {
+			// Reverse combined chars
+			reverse(characters[i:j], j-i)
+		}
 
-    // Reverse it
-    reverse(characters, charsLength)
+		i = j
+	}
 
-    return string(characters)
+	// Reverse it
+	reverse(characters, charsLength)
+
+	return string(characters)
 }
 
 func reverse(runes []rune, length int) {
-    for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
 }
 
 func isAMarker(r rune) bool {
-    return unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r)
+	return unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/gsora/garbagetextbot
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/gsora/nibberbot v0.0.0-20171017171541-8a846b04af2e
+	github.com/stretchr/testify v1.4.0
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
